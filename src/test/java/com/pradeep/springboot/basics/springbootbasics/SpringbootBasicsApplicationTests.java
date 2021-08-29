4e869de15a2bbf6db0049a4eadccb99a4974996f
Pradeep B <{ID}+{username}@users.noreply.github.com>
2021-08-29 11:52:07 +0530

    First commit

---
+++ src/test/java/com/pradeep/springboot/basics/springbootbasics/SpringbootBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pradeep.springboot.basics.springbootbasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
